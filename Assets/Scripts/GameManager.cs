d18b88dfc2d0fd30845d865f2778bd4b10be850e
Trysac <jmgomezsac@gmail.com>
2022-01-13 11:15:07 -0500

    GameManager Basics

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("General Parameters")]
+    [SerializeField] int playerHealth;
+    [SerializeField] int playerMoney;
+
+    [Header("Components")]
+    [SerializeField] TextMeshProUGUI playerHealthAndMoneyText;
+
+    [Header("Events")]
+    [SerializeField] UnityEvent onEnemyDestroyed;
+    [SerializeField] UnityEvent onMoneyChanged;
+
+    //Singleton
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance) { Destroy(this.gameObject); }
+        else { instance = this; }
+    }
+    public void UpdateHealthAndMoneyText() {
+        playerHealthAndMoneyText.text = $"Health: {playerHealth}\nMoney: ${playerMoney}";
+        //playerHealthAndMoneyText.text = "Health: " + playerHealth + "\nMoney:" + playerMoney;
+    }
+
+    public void AddMoney(int amount)
+    {
+        playerMoney += amount;
+        UpdateHealthAndMoneyText();
+        onMoneyChanged.Invoke();
+    }
+
+    public void TakeMoney(int amount)
+    {
+        playerMoney += amount;
+        UpdateHealthAndMoneyText();
+        onMoneyChanged.Invoke();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        playerHealth -= amount;
+        UpdateHealthAndMoneyText();
+        if (playerHealth <= 0) { GameOver(); }
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void WinGame()
+    {
+
+    }
+
+    public void OnEnemyDestroyed()
+    {
+
+    }
+}
